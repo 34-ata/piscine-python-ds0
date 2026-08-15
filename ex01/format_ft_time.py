d8a6966e1f6c1989d191c095b226c1b7d2a31d21
Fatih Mehmet Ata <faata@k1m14s01.42istanbul.com.tr>
2026-08-15 15:51:15 +0300

    feat(ex01): implement format_ft_time.py

---
+++ ex01/format_ft_time.py
@@ -0,0 +1,7 @@
+import time
+
+now = time.time()
+
+print(f"Seconds since January 1, 1970: {now:,.4f} or {now:.2e} in scientific notation")
+formatted = time.strftime("%b %d %Y", time.localtime(now))
+print(formatted)
